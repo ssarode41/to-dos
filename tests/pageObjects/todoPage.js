712a79739d575007026b4ac9998778b6f597ccd3
ssarode41 <sanketrajendra_sarode@epam.com>
2026-08-12 01:44:59 +0530

    SCRUM-442 update Playwright page object to support metadata fields and edit flow

--- tests/pageObjects/todoPage.js
+++ tests/pageObjects/todoPage.js
@@ -6,6 +6,9 @@ class TodoPage {
     this.data = data;
     this.titleInput = page.getByLabel('Todo title');
     this.descriptionInput = page.getByLabel('Todo description');
+    this.prioritySelect = page.getByLabel('Todo priority');
+    this.categorySelect = page.getByLabel('Todo category');
+    this.dueDateInput = page.getByLabel('Todo due date');
     this.saveButton = page.getByRole('button', { name: 'Save' });
   }
 
@@ -17,8 +20,49 @@ class TodoPage {
   async createTodo() {
     await this.titleInput.fill(this.data.title);
     await this.descriptionInput.fill(this.data.description);
+
+    if (this.data.priority) {
+      await this.prioritySelect.selectOption(this.data.priority);
+    }
+
+    if (this.data.category) {
+      await this.categorySelect.selectOption(this.data.category);
+    }
+
+    if (this.data.dueDate) {
+      await this.dueDateInput.fill(this.data.dueDate);
+    }
+
     await this.saveButton.click();
     await expect(this.page.getByText(this.data.title)).toBeVisible();
+  }
+
+  async startEdit() {
+    await this.page.getByRole('button', { name: 'Edit' }).first().click();
+  }
+
+  async cancelEdit() {
+    await this.page.getByRole('button', { name: 'Cancel' }).click();
+  }
+
+  async saveEdit(updated) {
+    await this.titleInput.fill(updated.title);
+    await this.descriptionInput.fill(updated.description);
+
+    if (updated.priority) {
+      await this.prioritySelect.selectOption(updated.priority);
+    }
+
+    if (updated.category) {
+      await this.categorySelect.selectOption(updated.category);
+    }
+
+    if (updated.dueDate) {
+      await this.dueDateInput.fill(updated.dueDate);
+    }
+
+    await this.saveButton.click();
+    await expect(this.page.getByText(updated.title)).toBeVisible();
   }
 }
 
