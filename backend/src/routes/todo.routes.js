828ce85a8dd6e1b23e9e478c2aef9eb7387a3b50
ssarode41 <sanketrajendra_sarode@epam.com>
2026-08-12 00:53:52 +0530

    SCRUM-427 Validate list query params and add reopen route

--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const { createTodo, listTodos, getTodo, updateTodo, deleteTodo, completeTodo } = require('../controllers/todo.controller');
+const { createTodo, listTodos, getTodo, updateTodo, deleteTodo, completeTodo, reopenTodo } = require('../controllers/todo.controller');
 const validate = require('../middlewares/validation');
 const { createTodoSchema, updateTodoSchema } = require('../validators/todo.validator');
+const { todoQuerySchema } = require('../validators/todoQuery.validator');
 
 const router = express.Router();
 
-router.get('/', listTodos);
+router.get('/', validate(todoQuerySchema, 'query'), listTodos);
 router.get('/:id', getTodo);
 router.post('/', validate(createTodoSchema), createTodo);
 router.put('/:id', validate(updateTodoSchema), updateTodo);
 router.patch('/:id/complete', completeTodo);
+router.patch('/:id/reopen', reopenTodo);
 router.delete('/:id', deleteTodo);
 
 module.exports = router;
