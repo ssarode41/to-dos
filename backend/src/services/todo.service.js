5769fb9d63f2b656942c584ed64ee6155a9b3d6b
ssarode41 <sanketrajendra_sarode@epam.com>
2026-08-12 00:52:48 +0530

    SCRUM-427 Pass query params to listTodos

--- backend/src/services/todo.service.js
+++ backend/src/services/todo.service.js
@@ -2,9 +2,9 @@ const todoRepository = require('../repositories/todo.repository');
 const logger = require('../config/logger');
 
 class TodoService {
-  async listTodos() {
+  async listTodos(query = {}) {
     logger.info('Listing todos');
-    return todoRepository.list();
+    return todoRepository.list(query);
   }
 
   async getTodoById(id) {
@@ -43,6 +43,16 @@ class TodoService {
 
   async completeTodo(id) {
     const todo = await todoRepository.complete(id);
+    if (!todo) {
+      const error = new Error('Todo not found');
+      error.statusCode = 404;
+      throw error;
+    }
+    return todo;
+  }
+
+  async reopenTodo(id) {
+    const todo = await todoRepository.reopen(id);
     if (!todo) {
       const error = new Error('Todo not found');
       error.statusCode = 404;
