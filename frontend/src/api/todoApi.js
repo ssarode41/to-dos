c595bd45468f134c378a1a747d7f4d9c5a563d58
ssarode41 <sanketrajendra_sarode@epam.com>
2026-08-11 18:37:06 +0530

    SCRUM-398: allow passing query params to getTodos

--- frontend/src/api/todoApi.js
+++ frontend/src/api/todoApi.js
@@ -1,6 +1,6 @@
 import apiClient from './apiClient';
 
-export const getTodos = () => apiClient.get('/todos');
+export const getTodos = (params) => apiClient.get('/todos', { params });
 export const createTodo = (payload) => apiClient.post('/todos', payload);
 export const updateTodo = (id, payload) => apiClient.put(`/todos/${id}`, payload);
 export const completeTodo = (id) => apiClient.patch(`/todos/${id}/complete`);
